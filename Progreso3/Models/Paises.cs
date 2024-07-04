7ccd33600d1defd9e71c36ee98b495a4d816e980
Strongartt <rikardoperez0809@gmail.com>
2024-07-04 07:17:28 -0500

    Agregar archivos de proyecto.

---
+++ Progreso3/Models/Paises.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progreso3.Models
+{
+    public class Paises
+    {
+        public string name {  get; set; }
+        public string region { get; set; }
+        public string subregion { get; set; }
+        public string status { get; set; }
+
+    }
+}
---
+++ Progreso3/repositories/PaisesRepositories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progreso3.repositories
+{
+    public class PaisesRepositories
+    {
+        public string _dbPatch;
+
+    }
+}
